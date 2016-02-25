2b957d37d5f4fc9f73d845230041f7773a3f16c6
qili.mai <517184297@qq.com>
2016-02-25 17:39:06 +0800

    Initial Commit

---
+++ QLLabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QLLabelDemo
+//
+//  Created by maiqili on 16/2/25.
+//  Copyright © 2016年 maiqili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QLLabelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QLLabelDemo
+//
+//  Created by maiqili on 16/2/25.
+//  Copyright © 2016年 maiqili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
